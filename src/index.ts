c2a5f087ea0a28f97f50410fc3933bc80d744a4b
Lanny MacMillan <lannymacmillan@gmail.com>
2025-02-21 00:32:03 -0500

    reconfigured tsup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
-export { generateTokens, verifyToken, verifyRefreshToken } from './jwt';
-export { authenticateToken as hapiAuth } from './middleware/hapi';
-export { authenticateToken as nextAuth } from './middleware/next';
-export { AuthenticateToken as nestAuth } from './middleware/nest';
-export { authorizeRoles as rbacAuth } from './middleware/rbac';
-export { authenticateToken as expressAuth } from './middleware/express';
-export { authenticateToken as fastifyAuth } from './middleware/fastify';
-export { authenticateToken as koaAuth } from './middleware/koa';
+import { generateTokens, verifyToken, verifyRefreshToken } from './jwt';
+import { authenticateToken as hapiAuth } from './middleware/hapi';
+import { authenticateToken as nextAuth } from './middleware/next';
+import { AuthenticateToken as nestAuth } from './middleware/nest';
+import { authorizeRoles as rbacAuth } from './middleware/rbac';
+import { authenticateToken as expressAuth } from './middleware/express';
+import { authenticateToken as fastifyAuth } from './middleware/fastify';
+import { authenticateToken as koaAuth } from './middleware/koa';
+
+export {
+  generateTokens,
+  verifyToken,
+  verifyRefreshToken,
+  hapiAuth,
+  nextAuth,
+  nestAuth,
+  rbacAuth,
+  expressAuth,
+  fastifyAuth,
+  koaAuth,
+};
+
+export default {
+  generateTokens,
+  verifyToken,
+  verifyRefreshToken,
+  hapiAuth,
+  nextAuth,
+  nestAuth,
+  rbacAuth,
+  expressAuth,
+  fastifyAuth,
+  koaAuth,
+};
